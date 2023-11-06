1d5e8c476ddac9c6e162057dbda0b201063ef133
Ajibola <101070743+Nathyshegs@users.noreply.github.com>
2023-11-05 23:18:52 -0500

    added the main files and others

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# #variables.tf
+# variable "access_key" {
+#     description = "Access key to AWS console"
+# }
+# variable "secret_key" {
+#     description = "Secret key to AWS console"
+# }
+variable "region" {
+    description = "AWS region"
+}
+
+variable "ca_cert_identifier" {
+    description = "ca_cert_identifier"
+}
